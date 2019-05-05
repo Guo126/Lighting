96cdd778f3275243ef607b5096a77ec6cb8486eb
Guo126 <40754088+Guo126@users.noreply.github.com>
2019-05-05 12:44:10 +0800

    nenewnew

--- app/src/main/java/com/dianmo/flash/ChatActivity.java
+++ app/src/main/java/com/dianmo/flash/ChatActivity.java
@@ -3,15 +3,22 @@ package com.dianmo.flash;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ChatActivity extends AppCompatActivity {
     private TextView name ;
+    private ImageView oprate,more;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
+        setContentView(R.layout.activity_chatt);
+
         name =(TextView) findViewById(R.id.name);
         name.setText(getIntent().getStringExtra("name"));
+        oprate = (ImageView)findViewById(R.id.oprate);
+        more = (ImageView)findViewById(R.id.more);
+
     }
 }
---
+++ app/src/main/java/com/dianmo/view/AudioButton.java
@@ -0,0 +1,16 @@
+package com.dianmo.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+public class AudioButton extends android.support.v7.widget.AppCompatButton {
+
+    public AudioButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AudioButton(Context context) {
+        this(context,null);
+    }
+}
